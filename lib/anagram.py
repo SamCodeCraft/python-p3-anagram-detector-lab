0781de98ccaf6eb518f2818162e7744f3a82ef18
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-26 12:10:39 +0300

    Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word) == sorted_word]
+
+# Example usage:
+finder = Anagram("finder")
+print(finder.match(['friend', 'finder', 'refind', 'fender', 'denrif']))
+# => ['friend', 'refind', 'denrif']
